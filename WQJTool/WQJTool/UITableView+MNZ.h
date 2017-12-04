54dae12cc807a923af860be420ec649a458d920a
王全金 <wangquanjin@rongzi.com>
2017-12-04 13:54:14 +0800

    0.1.2

---
+++ WQJTool/WQJTool/UITableView+MNZ.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+MNZ.h
+//  WQJTool
+//
+//  Created by 王全金 on 2017/12/4.
+//  Copyright © 2017年 mnz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (MNZ)
+
+@end
